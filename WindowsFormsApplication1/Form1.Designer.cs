59c2cee87a4a0ae10e92223af6850f82d5c9fc97
zaki <zaki@ZAKI-7-LAPTOP>
2017-02-22 16:41:30 +0200

    test1

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApplication1
 {
-    partial class Form1
+    partial class Start
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.username_label = new System.Windows.Forms.Label();
+            this.password_label = new System.Windows.Forms.Label();
+            this.username_textbox = new System.Windows.Forms.TextBox();
+            this.password_textBox = new System.Windows.Forms.TextBox();
+            this.Login_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // username_label
+            // 
+            this.username_label.AutoSize = true;
+            this.username_label.Location = new System.Drawing.Point(30, 37);
+            this.username_label.Name = "username_label";
+            this.username_label.Size = new System.Drawing.Size(53, 13);
+            this.username_label.TabIndex = 0;
+            this.username_label.Text = "username";
+            // 
+            // password_label
+            // 
+            this.password_label.AutoSize = true;
+            this.password_label.Location = new System.Drawing.Point(33, 78);
+            this.password_label.Name = "password_label";
+            this.password_label.Size = new System.Drawing.Size(52, 13);
+            this.password_label.TabIndex = 1;
+            this.password_label.Text = "password";
+            // 
+            // username_textbox
+            // 
+            this.username_textbox.Location = new System.Drawing.Point(109, 37);
+            this.username_textbox.Name = "username_textbox";
+            this.username_textbox.Size = new System.Drawing.Size(188, 20);
+            this.username_textbox.TabIndex = 2;
+            // 
+            // password_textBox
+            // 
+            this.password_textBox.Location = new System.Drawing.Point(109, 78);
+            this.password_textBox.Name = "password_textBox";
+            this.password_textBox.PasswordChar = '*';
+            this.password_textBox.Size = new System.Drawing.Size(188, 20);
+            this.password_textBox.TabIndex = 3;
+            // 
+            // Login_button
+            // 
+            this.Login_button.Location = new System.Drawing.Point(109, 127);
+            this.Login_button.Name = "Login_button";
+            this.Login_button.Size = new System.Drawing.Size(103, 23);
+            this.Login_button.TabIndex = 4;
+            this.Login_button.Text = "Login";
+            this.Login_button.UseVisualStyleBackColor = true;
+            this.Login_button.Click += new System.EventHandler(this.Login_button_Click);
+            // 
+            // Start
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(311, 162);
+            this.Controls.Add(this.Login_button);
+            this.Controls.Add(this.password_textBox);
+            this.Controls.Add(this.username_textbox);
+            this.Controls.Add(this.password_label);
+            this.Controls.Add(this.username_label);
+            this.Name = "Start";
+            this.Text = "Start";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label username_label;
+        private System.Windows.Forms.Label password_label;
+        private System.Windows.Forms.TextBox username_textbox;
+        private System.Windows.Forms.TextBox password_textBox;
+        private System.Windows.Forms.Button Login_button;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -10,11 +10,23 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApplication1
 {
-    public partial class Form1 : Form
+    public partial class Start : Form
     {
-        public Form1()
+        public Start()
         {
             InitializeComponent();
+        }
+
+        private void Login_button_Click(object sender, EventArgs e)
+        {
+            if(username_textbox.Text.Equals("Zaki") && password_textBox.Text.Equals("123"))
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("Wrong username/password !!");
+            }
         }
     }
 }
